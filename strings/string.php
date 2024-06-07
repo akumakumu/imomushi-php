41380672e1a25e3e77d5141a6df9da3096464c07
Yy <akumakumu@gmail.com>
2024-06-08 00:55:10 +0700

    string manip

--- strings/string.php
+++ strings/string.php
@@ -2,9 +2,28 @@
     // There is a big difference between double quotes and single quotes in PHP.
     // Double quotes process special characters, single quotes does not.
 
-    $x = "Mom";
+    $x = "Hello Mom";
     echo "Hello $x";
     echo 'Hello $x';
 
-    echo strlen($x)
+    // String Length
+    echo strlen($x);
+
+    // Word Count
+    echo str_word_count($x);
+
+    // Search for Text
+    echo strpos($x, "Mom");
+
+    // Upper Case
+    echo strtoupper($x);
+
+    // Lower Case
+    echo strtolower($x);
+    
+    // Replace
+    echo str_replace("Mom", "Ame", $x);
+
+    // Concat
+    
 ?>
