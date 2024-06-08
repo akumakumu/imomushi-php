c810fb9768bb88b166e7fc4f95f73827366d0686
Yy <akumakumu@gmail.com>
2024-06-08 22:24:35 +0700

    more strings

--- strings/string.php
+++ strings/string.php
@@ -25,5 +25,41 @@
     echo str_replace("Mom", "Ame", $x);
 
     // Concat
-    
+    $a = "Hello";
+    $b = "Mom";
+
+    $c = $a . $b;
+    $d = $a . " " . $b;
+    $e = "$a $b";
+
+    echo $e;
+
+    // Slicing
+    // Slice at index 6 and end 5 positions later
+    echo substr($x, 6, 5);
+
+    // Slice to the end from index 6
+    echo substr($x, 6);
+
+    // Slice From the End
+    // The last character has index -1
+    echo substr($x, -3, 3);
+
+    echo substr($x, 3, -2);
+
+    // Escape Character
+    // An escape character is a backslash \ followed by the character you want to insert.
+
+    $y = "Hello \"Mom\" ";
+
+    // Other Escape Characters
+    // Code     Res
+    // \'       Single Quote
+    // \$       PHP Variables
+    // \n       New Line
+    // \r       Carriage Return
+    // \t       Tab
+    // \f       Form Feed
+    // \ooo     Octal Value
+    // \xhh     Hex Value
 ?>
